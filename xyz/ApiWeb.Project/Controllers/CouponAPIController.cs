ac6a3c27930ac6911c80b20df1a5b8a54c68c109
jadhavpratik5433 <jadhavpratik5433@gmail.com>
2026-01-20 14:05:06 +0530

    add controller

---
+++ xyz/ApiWeb.Project/Controllers/CouponAPIController.cs
@@ -0,0 +1,33 @@
+﻿using ApiWeb.Project.Data;
+using ApiWeb.Project.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWeb.Project.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponAPIController : ControllerBase
+    {
+        private readonly AppDbContext _db;
+        public CouponAPIController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public object Get()
+        {
+            try
+            {
+                IEnumerable<Coupon> objList = _db.Coupons.ToList();
+                return objList;
+            }
+            catch(Exception ex)
+            {
+
+            }
+            return null;
+        }
+    }
+}
